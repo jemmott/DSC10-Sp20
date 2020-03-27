d2172d0fd33633e10da16b45370f841ac01730d8
cjemmott <cjemmott@seismic.com>
2020-03-27 15:26:17 -0700

    Lab01

---
+++ Labs/Lab01/tests/q721.py
@@ -0,0 +1,22 @@
+test = {
+  'name': '7.2.1',
+  'points': 1,
+  'suites': [
+    {
+      'cases': [
+        {
+          'code': r"""
+          >>> abs(difference)
+          30
+          """,
+          'hidden': False,
+          'locked': False
+        }
+      ],
+      'scored': True,
+      'setup': '',
+      'teardown': '',
+      'type': 'doctest'
+    }
+  ]
+}
---
+++ Labs/Lab01/tests/q731.py
@@ -0,0 +1,22 @@
+test = {
+  'name': '7.3.1',
+  'points': 1,
+  'suites': [
+    {
+      'cases': [
+        {
+          'code': r"""
+          >>> sentence_length
+          896
+          """,
+          'hidden': False,
+          'locked': False
+        }
+      ],
+      'scored': True,
+      'setup': '',
+      'teardown': '',
+      'type': 'doctest'
+    }
+  ]
+}
